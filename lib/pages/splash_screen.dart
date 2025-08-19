67cb657249e3b3982fc8acbdb9739684189c8ebf
adnan ahmed <adnanahmed504423@gmail.com>
2025-08-19 22:41:03 +0530

    adding shared preferences for storing status

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future<void> _navigateTotext()async{
+    await Future.delayed(Duration(seconds: 5));
+    final prefs= await SharedPreferences.getInstance();
+    bool isFirstTime=prefs.getBool('isFirstTime')?? true;
+  
+  if(!mounted) return;
+  if(isFirstTime){
+    await prefs.setBool('isFirstTime', false);
+
+    Navigator.pushReplacementNamed(context, '/signupscreen');
+  }
+    else{
+      bool isLoggedIn =prefs.getBool('isLoggedIn')?? false;
+      String? userToken=prefs.getString('userToken');
+      if(isLoggedIn && userToken!=null){
+        Navigator.pushReplacementNamed(context, '/homescreen');
+      }else{
+        Navigator.pushReplacementNamed(context, '/loginscreen');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Splash Screen"),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text('Splash Screen'),
+          )
+        ],
+      ),
+    );
+  }
+}
